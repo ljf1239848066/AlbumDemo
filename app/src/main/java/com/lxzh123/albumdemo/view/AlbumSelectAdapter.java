c409eb7e14655f442e1f1f48321ad75f40eca620
ljf1239848066 <1239848066@qq.com>
2018-08-22 19:07:56 +0800

    init

--- app/src/main/java/com/lxzh123/albumdemo/view/AlbumSelectAdapter.java
+++ app/src/main/java/com/lxzh123/albumdemo/view/AlbumSelectAdapter.java
@@ -1,40 +1,56 @@
 package com.lxzh123.albumdemo.view;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.SimpleAdapter;
+import android.widget.BaseAdapter;
 
 import java.util.List;
-import java.util.Map;
 
 /**
  * description 自定义相册选择器适配器
  * author      Created by lxzh
  * date        2018/8/22
  */
-public class AlbumSelectAdapter extends SimpleAdapter {
-    public AlbumSelectAdapter(Context context, List<? extends Map<String, ?>> data, int resource, String[] from, int[] to) {
-        super(context, data, resource, from, to);
+public class AlbumSelectAdapter extends BaseAdapter {
+    private Context context;
+    private List<GridBean> dataList;
+
+    public AlbumSelectAdapter(Context context, List<GridBean> data) {
+        this.context=context;
+        this.dataList=data;
     }
 
     @Override
     public int getCount() {
-        return super.getCount();
+        return dataList.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return super.getItem(position);
+        return dataList.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return super.getItemId(position);
+        return position;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return super.getView(position, convertView, parent);
+        ViewHolder viewHolder;
+        if(convertView==null){
+//            LayoutInflater inflater=(LayoutInflater.from(context));
+            LayoutInflater inflater=(LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }else{
+
+        }
+
+        return convertView;
+    }
+
+    static class ViewHolder{
+        AlbumSelectGridView gridView;
     }
 }
